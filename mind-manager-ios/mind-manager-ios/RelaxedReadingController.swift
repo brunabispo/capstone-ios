00f0213f39e2634494861a6b76629b0cc54bb173
brunabispo <bruna.abatepaulo@hotmail.com>
2021-04-05 20:46:35 -0300

    details of books for relaxed

--- mind-manager-ios/mind-manager-ios/RelaxedReadingController.swift
+++ mind-manager-ios/mind-manager-ios/RelaxedReadingController.swift
@@ -25,65 +25,59 @@ class RelaxedReadingController: UIViewController{
         viewDidLoad().self
     }
     
-    
-    
-    
-    var relaxedReading = ["The Adventures of Tom Sawyer", "The Little Book of Mindfulness", "The Art of Thinking Clearly", "The Girl on the Train", "How the Penguins Saved Veronica", "The Snow Child", "Why Not Me?", "Fantastic Mr Fox", "The Little Prince", "The Little Prince"]
-    
-    
     override func viewDidLoad() {
         lblBookTitle?.text = relaxedSuggestion.relaxedReading.randomElement()
         
         switch lblBookTitle.text {
         case "The Adventures of Tom Sawyer":
-            lblAuthor?.text = ""
-            lblYear?.text = ""
-            lblNoPages?.text = ""
+            lblAuthor?.text = "Mark Twain"
+            lblYear?.text = "1876"
+            lblNoPages?.text = "216"
             imageBook.image = UIImage(named: "tom_sawyer")
         case "The Little Book of Mindfulness":
-            lblAuthor?.text = ""
-            lblYear?.text = ""
-            lblNoPages?.text = ""
+            lblAuthor?.text = "Patrizia Collard"
+            lblYear?.text = "2014"
+            lblNoPages?.text = "96"
             imageBook.image = UIImage(named: "little_book")
         case "The Art of Thinking Clearly":
-            lblAuthor?.text = ""
-            lblYear?.text = ""
-            lblNoPages?.text = ""
+            lblAuthor?.text = "Rolf Dobelli"
+            lblYear?.text = "2011"
+            lblNoPages?.text = "326"
             imageBook.image = UIImage(named: "think")
         case "The Girl on the Train":
-            lblAuthor?.text = ""
-            lblYear?.text = ""
-            lblNoPages?.text = ""
+            lblAuthor?.text = "Paula Hawkins"
+            lblYear?.text = "2015"
+            lblNoPages?.text = "317"
             imageBook.image = UIImage(named: "girl_train")
         case "How the Penguins Saved Veronica":
-            lblAuthor?.text = ""
-            lblYear?.text = ""
-            lblNoPages?.text = ""
+            lblAuthor?.text = "Hazel Prior"
+            lblYear?.text = "2020"
+            lblNoPages?.text = "368"
             imageBook.image = UIImage(named: "veronica")
         case "The Snow Child":
-            lblAuthor?.text = ""
-            lblYear?.text = ""
-            lblNoPages?.text = ""
+            lblAuthor?.text = "Eowyn Ivey"
+            lblYear?.text = "2011"
+            lblNoPages?.text = "400"
             imageBook.image = UIImage(named: "snow")
         case "Why Not Me?":
-            lblAuthor?.text = ""
-            lblYear?.text = ""
-            lblNoPages?.text = ""
+            lblAuthor?.text = "Mindy Kaling"
+            lblYear?.text = "2015"
+            lblNoPages?.text = "240"
             imageBook.image = UIImage(named: "why")
         case "Fantastic Mr Fox":
-            lblAuthor?.text = ""
-            lblYear?.text = ""
-            lblNoPages?.text = ""
+            lblAuthor?.text = "Roald Dahl"
+            lblYear?.text = "1970"
+            lblNoPages?.text = "96"
             imageBook.image = UIImage(named: "fox")
         case "The Little Prince":
-            lblAuthor?.text = ""
-            lblYear?.text = ""
-            lblNoPages?.text = ""
+            lblAuthor?.text = "Antoine de Saint-Exupéry"
+            lblYear?.text = "1943"
+            lblNoPages?.text = "96"
             imageBook.image = UIImage(named: "prince")
         case "Attachments":
-            lblAuthor?.text = ""
-            lblYear?.text = ""
-            lblNoPages?.text = ""
+            lblAuthor?.text = "Rainbow Rowell"
+            lblYear?.text = "2011"
+            lblNoPages?.text = "323"
             imageBook.image = UIImage(named: "attach")
         default:
             lblAuthor?.text = ""
